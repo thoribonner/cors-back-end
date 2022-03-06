ffb5a2c98908019d138b185620f7404ec12acf5a
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-03-06 12:58:36 -0500

    init commit

---
+++ src/cors-enabled/cors-enabled.router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const controller = require("./cors-enabled.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+router
+  .route("/:corsId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+
+router
+  .route("/")
+  .get(cors(), controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+module.exports = router;
